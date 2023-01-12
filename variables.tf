2aaa4318c5cac44f6cc59b3362f6582e5a34efb8
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-01-12 20:34:21 +0100

    Fixing duplicate variable

--- variables.tf
+++ variables.tf
@@ -53,21 +53,3 @@ variable identity_ids {
     type = list
     default = null
 }
-
-variable "default_tags_enabled" {
-    description = "Option to enable or disable default tags."
-    type        = bool
-    default     = true
-}
-
-variable "default_tags" {
-  description = "Base tags common to all resources"
-  type        = map(string)
-  default     = {}
-}
-
-variable "extra_tags" {
-  description = "Additional tags to associate with your Static site."
-  type        = map(string)
-  default     = {}
-}

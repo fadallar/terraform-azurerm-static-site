721f9692b4f1dda3f0d08802a3f6990c3bdef1cd
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-01-12 20:15:06 +0100

    Adding missing variable

--- locals-naming.tf
+++ locals-naming.tf
@@ -3,5 +3,5 @@ locals {
   name_prefix = lower(var.name_prefix)
   name_suffix = lower(var.name_suffix)
 
-  static_site_name = coalesce(var.custom_name, lower(data.azurecaf_name.static_site.result))
+  static_site_name = coalesce(var.static_site_custom_name, lower(data.azurecaf_name.static_site.result))
 }

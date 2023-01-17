61add56a20cf773935cd51513a7ec62ec0d5e493
Fabrice Dallara <fabrice.dallara@hotmail.com>
2023-01-17 12:39:38 +0100

    Terraform fmt

--- staticsite.tf
+++ staticsite.tf
@@ -4,12 +4,12 @@ resource "azurerm_static_site" "static_site" {
   location            = var.location
   sku_tier            = var.sku_tier
   sku_size            = var.sku_size
-  
+
   dynamic "identity" {
     for_each = var.identity_type == null ? [] : ["enabled"]
     content {
-    type         = var.identity_type
-    identity_ids = var.identity_ids == "UserAssigned" ? var.identity_ids : null
+      type         = var.identity_type
+      identity_ids = var.identity_ids == "UserAssigned" ? var.identity_ids : null
     }
   }
   tags = merge(var.default_tags, var.extra_tags)
